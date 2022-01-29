3276f7bee5458f1e555433bd0b591b93525010ad
Jay Panchal <panchalj272@gmail.com>
2022-01-29 18:11:39 +0530

    Adding starter project

--- game-frontend/src/App.js
+++ game-frontend/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
+import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
 
-function App() {
+// Constants
+const TWITTER_HANDLE = '_buildspace';
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="header-container">
+          <p className="header gradient-text">⚔️ Metaverse Slayer ⚔️</p>
+          <p className="sub-text">Team up to protect the Metaverse!</p>
+          <div className="connect-wallet-container">
+            <img
+              src="https://64.media.tumblr.com/tumblr_mbia5vdmRd1r1mkubo1_500.gifv"
+              alt="Monty Python Gif"
+            />
+          </div>
+        </div>
+        <div className="footer-container">
+          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+          <a
+            className="footer-text"
+            href={TWITTER_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{`built with @${TWITTER_HANDLE}`}</a>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
